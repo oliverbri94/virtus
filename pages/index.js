cfe4b7c4ad79fdaffc9c065faaf0ace9a0d61ed1
aromotorsa <amerovictorialed@gmail.com>
2025-10-12 18:14:43 -0500

    asdasd

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,19 @@
 // pages/index.js
-import Head from 'next/head';
-import Navbar from '../components/Navbar';
-import HeroConPlexo from '../components/HeroConPlexo';
-import Soluciones from '../components/Soluciones'; // <-- Sabemos que este es inocente
-import Footer from '../components/Footer';
-
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+// ... (imports)
+import Metodologia from '../components/Metodologia'; // <-- Añadimos al sospechoso
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Base Segura - Virtus</title>
-      </Head>
-      <Navbar />
+      {/* ... Head y Navbar ... */}
       <main>
         <HeroConPlexo />
         <Soluciones />
+        <Resultados />
+        <Metodologia /> {/* <--- ¡EL SIGUIENTE SOSPECHOSO! */}
       </main>
       <Footer />
     </div>
   );
 }
-
-export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale ?? 'es', ['common'])),
-    },
-  };
-}
+// ... (getStaticProps)
