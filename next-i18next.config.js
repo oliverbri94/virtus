969b40d506ff68e0d74166d39d1074be08d371fd
aromotorsa <amerovictorialed@gmail.com>
2025-10-12 22:34:17 -0500

    asdasd

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
+    // Estos son todos los idiomas que soportas en tu sitio.
+    locales: ['en', 'es'],
+    // Este es el idioma por defecto que se usará si el navegador del usuario
+    // no coincide con ninguno de los idiomas soportados.
+    defaultLocale: 'en',
   },
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
