77f843fd56ef3917915e3ecbd50f3453adf9263b
aromotorsa <amerovictorialed@gmail.com>
2025-10-13 15:49:44 -0500

    asdad

--- components/HeroConPlexo.js
+++ components/HeroConPlexo.js
@@ -23,8 +23,8 @@ const HeroConPlexo = () => {
 
       {/* Divisor de sección con forma SVG */}
       <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="relative block w-full h-[75px]">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="fill-gray-50"></path>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="relative block w-full h-[100px]">
+          <path d="M0,60 C200,150 350,0 600,60 C850,120 1000,-20 1200,60 L1200,120 L0,120 Z" className="fill-gray-50"></path>
         </svg>
       </div>
     </section>
