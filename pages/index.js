b113086f81bee1a2793a0a0e774ad0e308d54b06
aromotorsa <amerovictorialed@gmail.com>
2025-10-12 20:41:39 -0500

    asdasds!

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,41 @@
 // pages/index.js
-// ... (imports)
-import Resultados from '../components/Resultados'; // <-- Asegúrate que esté aquí
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import HeroConPlexo from '../components/HeroConPlexo';
+import Soluciones from '../components/Soluciones';
+import Resultados from '../components/Resultados';
+import Metodologia from '../components/Metodologia';
+import BlogSection from '../components/BlogSection';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import WhatsAppButton from '../components/WhatsAppButton';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export default function Home() {
   return (
     <div>
-      {/* ... Head y Navbar ... */}
+      <Head>
+        <title>Virtus Tech Consulting - Automatización y RPA en Ecuador</title>
+      </Head>
+      <Navbar />
       <main>
         <HeroConPlexo />
         <Soluciones />
-        <Resultados /> {/* <--- ¡EL SOSPECHOSO ESTÁ EN LA SALA! */}
-        {/* Podemos dejar los otros comentados por ahora para ir más rápido */}
-        {/* <Metodologia /> */}
-        {/* <BlogSection /> */}
-        {/* <Contact /> */}
+        <Resultados />
+        <Metodologia />
+        <BlogSection />
+        <Contact />
       </main>
+      <WhatsAppButton />
       <Footer />
     </div>
   );
 }
-// ... (getStaticProps)
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? 'es', ['common'])),
+    },
+  };
+}
